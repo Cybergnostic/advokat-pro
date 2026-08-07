1bee87ef501952ab3540df0ad7530a0e1add6f8d
Cybergnostic <Cybergnostic@users.noreply.github.com>
2026-08-07 09:00:01 +0200

    Load shared D1 state on app startup

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 // INIT
-initSW();
-checkBanner();
-checkAlarms();
-scheduleAlarms();
-pVrsta();
-render();
+async function initApp(){
+  initSW();
+  checkBanner();
+  pVrsta();
+  try{
+    await loadSharedState();
+  }catch(e){
+    console.error('Shared database could not be loaded',e);
+    alert('Zajednička baza trenutno nije dostupna. Podaci nisu učitani.');
+  }
+  checkAlarms();
+  scheduleAlarms();
+  render();
+}
+initApp();
