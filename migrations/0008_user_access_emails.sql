035f3c61f279f2926bca0f5909f37e4de8fb3809
Cybergnostic <Cybergnostic@users.noreply.github.com>
2026-08-07 17:01:27 +0200

    Bind user profiles to Cloudflare Access emails

---
+++ migrations/0008_user_access_emails.sql
@@ -0,0 +1,4 @@
+UPDATE users SET access_email = 'adv.pro.app@gmail.com', updated_at = CURRENT_TIMESTAMP WHERE id = 'aleksa-biskup';
+UPDATE users SET access_email = 'advokatzvanija@gmail.com', updated_at = CURRENT_TIMESTAMP WHERE id = 'zvanija';
+UPDATE users SET access_email = 'gruja277@gmail.com', updated_at = CURRENT_TIMESTAMP WHERE id = 'gruja';
+UPDATE users SET access_email = 'cybergnostic6@gmail.com', updated_at = CURRENT_TIMESTAMP WHERE id = 'jovan-dev';
